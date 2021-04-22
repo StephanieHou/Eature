d3d338d099a39d1f2e0a6cca5ee13aba1e05713f
Stephanie Hou <stephaniehou@Stephanies-Air.lan>
2021-04-21 20:05:54 -0400

    Mapping

--- src/Components/Map/LeafletMap.js
+++ src/Components/Map/LeafletMap.js
@@ -9,7 +9,8 @@ const Container = styled.div`
   min-height: 700px;
 `
 
-const LeafletMap = () => {
+const LeafletMap = ({ data }) => {
+  const test = 51.505
   return (
     <Main className="Leaflet-Map">
       <Container className="Map" id="Eature-Map">
@@ -18,11 +19,23 @@ const LeafletMap = () => {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://api.mapbox.com/styles/v1/shou94/cknrxzbnx157017qo9oemv1uf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hvdTk0IiwiYSI6ImNrbnJ4dDg3ZTBtdm0yd3BoYXUzNXNsOTEifQ.aAUh-Nc0vIQMbbtWQfg0DQ"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {data &&
+            data.map((event, i) => (
+              <Marker
+                key={i}
+                position={[
+                  parseFloat(event.geometry[0].coordinates[0]),
+                  parseFloat(event.geometry[0].coordinates[0])
+                ]}
+              >
+                <Popup>
+                  {event.title}
+                  <br/>
+                  {parseFloat(event.geometry[0].coordinates[0])}{" "}
+                  {parseFloat(event.geometry[0].coordinates[1])}
+                </Popup>
+              </Marker>
+            ))}
         </MapContainer>
       </Container>
     </Main>
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import { Main, Sidebar } from "../Styled"
 import LeafletMap from "./LeafletMap"
@@ -8,11 +8,31 @@ const Container = styled.div`
 `
 
 const Map = () => {
+  const [events, setEvents] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
+      )
+
+      if (!response.ok) {
+        console.log(response.error)
+        return
+      }
+
+      const data = await response.json()
+      setEvents(data.events)
+    }
+    fetchData()
+  }, [])
+
   return (
     <Main className="Map-Landing">
       <Sidebar />
       <Container className="Map-Wrapper">
-        <LeafletMap />
+        <LeafletMap data={events} />
       </Container>
     </Main>
   )
